453c249e08d329db3c2130cc94d591d7019587d1
younes2808 <benhaida.mehdi@gmail.com>
2026-06-09 20:54:35 +0200

    He walking

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
-
+    private bool isWalking = false;
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -26,8 +26,11 @@ public class Player : MonoBehaviour
         inputVector = inputVector.normalized;
         Vector3 moveDir =  new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position +=  moveDir * moveSpeed * Time.deltaTime;
-
+        isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime* rotateSpeed);    
     }
+
+    public bool IsWalking() => isWalking;
+   
 }
